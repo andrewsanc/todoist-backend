2be0fb49b15296857277ba36923ff0410ed8c259
TB831 <druu831@gmail.com>
2021-11-17 11:25:47 -0800

    hello world route

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+// Routes
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+
 const port = 4000;
 
 app.listen(port, console.log(`Server is listening on port ${port}`));
