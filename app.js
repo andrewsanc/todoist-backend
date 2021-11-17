d81b2b6a6ecb04a604b1765a4492555a2bab8c8f
TB831 <druu831@gmail.com>
2021-11-17 11:24:27 -0800

    express app init. added local port 4000

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("Hello W0rld");
+const express = require("express");
+const app = express();
+
+const port = 4000;
+
+app.listen(port, console.log(`Server is listening on port ${port}`));
