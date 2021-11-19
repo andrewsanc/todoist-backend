f9ff90c3ef72fb0a45a1ae2fb689a83688466598
TB831 <druu831@gmail.com>
2021-11-19 15:14:36 -0800

    successful creation of task within our db via mongoose interface

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -5,9 +5,9 @@ const getAllTasks = (req, res) => {
   res.send("get all tasks");
 };
 
-const createTask = (req, res) => {
-  console.log(`received a request to create a new task`);
-  res.json({ success: "successfully created a task" });
+const createTask = async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
 };
 
 const getTask = (req, res) => {
