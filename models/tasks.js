a5737c36133e15ff652b814d831a98a955a496dc
TB831 <druu831@gmail.com>
2021-11-22 10:32:41 -0800

    added basic validation to our TaskSchema

--- models/tasks.js
+++ models/tasks.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "Must provide a name"], // required[1] - response if name value isn't provided
+    trim: true, // trims our req name value
+    maxlength: [20, "name cannot exceed 20 characters"], // Sets the maxLength of our name string and response if maxLength exceeds 20 chars
+  },
+  completed: {
+    type: Boolean,
+    default: false, // Default value for our db
+  },
 });
 
 module.exports = mongoose.model("Task", TaskSchema);
