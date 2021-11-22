a15308427a5657c098dee20a3f0b187a64e5a772
TB831 <druu831@gmail.com>
2021-11-22 11:21:58 -0800

    set up of our getTask endpoint

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -19,9 +19,20 @@ const createTask = async (req, res) => {
   }
 };
 
-const getTask = (req, res) => {
-  console.log(`requesting task ${req.params.id}`);
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+
+    if (!task) {
+      // if no id is found within our tasks collection
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const updateTask = (req, res) => {
